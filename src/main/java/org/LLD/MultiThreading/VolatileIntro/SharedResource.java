dce632f8f18df271c7bf52f7e2dee92528e84eae
Sana Abhishek <sanaabhishek8@gmail.com>
2025-02-12 02:00:41 +0300

    Added documentation for volatile

--- src/main/java/org/LLD/MultiThreading/VolatileIntro/SharedResource.java
+++ src/main/java/org/LLD/MultiThreading/VolatileIntro/SharedResource.java
@@ -2,7 +2,26 @@ package org.LLD.MultiThreading.VolatileIntro;
 
 public class SharedResource {
 
-    /// If a variable is volatile then all thread uses the variable from heap rather the thread cache
+
+    /**
+     * The volatile keyword is used as modifier for class variables.
+     * It's an indicator that this variable's value may be changed by multiple
+     * threads.
+     * This modifier ensures that the variable is always read from, and written to the
+     * main memory, rather than from any thread-specific cache.
+     * This provides memory consistency for this variables value across threads.
+     * Volatile doesn't gurantee atomicicty.
+     *
+     * However, volatile does not provide atomicity or synchronization, so additional
+     * synchronization mechanisms should be used in conjunction with it when necessary.
+     * When to use volatile
+     * When a variable is used to track the state of a shared resource, such as
+     * counter or a flag.
+     * When a varaible is used to communicate between threads.
+     * When not use volatile
+     * When the variable is used by single thread.
+     * When a variable is used to store a large amount of data.
+     */
     private volatile boolean flag;
 
     public SharedResource(){
