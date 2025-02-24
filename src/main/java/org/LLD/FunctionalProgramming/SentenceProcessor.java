a72c3d23fee891520af8047aec47c247a0c5e5a4
Sana Abhishek <sanaabhishek8@gmail.com>
2025-02-25 01:57:58 +0300

    Profitable path in a tree - DSA

---
+++ src/main/java/org/LLD/FunctionalProgramming/SentenceProcessor.java
@@ -0,0 +1,37 @@
+package org.LLD.FunctionalProgramming;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.OptionalDouble;
+
+public class SentenceProcessor {
+
+    public static int processSentences(List<String> sentences) {
+        //code here
+        double result = sentences.stream().filter(sentence -> sentence.contains("Java")).mapToInt(String::length)
+                .average().orElse(0);
+        return (int)result;
+    }
+
+    public static void main(String[] args) {
+        List<String> sentences = Arrays.asList(
+                "Java is a programming language.",
+                "Python is also a good language.",
+                "Java stream processing is powerful.",
+                "C++ is not as popular as Java."
+        );
+
+        int averageLength = SentenceProcessor.processSentences(sentences);
+        System.out.println(averageLength);
+
+        sentences = Arrays.asList(
+                "Python is a programming language.",
+                "JS is used for web development.",
+                "Ruby is known for its simplicity.",
+                "java is good"
+        );
+
+        averageLength = SentenceProcessor.processSentences(sentences);
+        System.out.println(averageLength);
+    }
+}
---
+++ src/main/java/org/LLD/FunctionalProgramming/WordCounter.java
@@ -0,0 +1,25 @@
+package org.LLD.FunctionalProgramming;
+
+import java.util.*;
+
+public class WordCounter {
+
+    public static long countWords(List<String> sentences) {
+        long words = sentences.stream().filter(sentence -> !sentence.contains("Java"))
+               .flatMap(sentence -> Arrays.stream(sentence.split(" ")))
+                .distinct()
+                .count();
+        return words;
+    }
+
+    public static void main(String[] args) {
+        List<String> sentences = Arrays.asList(
+                "Python is a programming language.",
+                "JavaScript is used for web development.",
+                "Ruby is known for its simplicity."
+        );
+
+        long wordCount = WordCounter.countWords(sentences);
+        System.out.println(wordCount);
+    }
+}
