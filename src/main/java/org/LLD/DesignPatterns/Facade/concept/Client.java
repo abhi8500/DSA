6d5bfdd6d15cd96d04971f4ad0b600504437e0e4
Sana Abhishek <sanaabhishek8@gmail.com>
2025-03-22 17:32:26 +0300

    Facade design pattern concept

--- src/main/java/org/LLD/DesignPatterns/Facade/concept/Client.java
+++ src/main/java/org/LLD/DesignPatterns/Facade/concept/Client.java
@@ -3,7 +3,14 @@ package org.LLD.DesignPatterns.Facade.concept;
 public class Client {
 
     public static void main(String[] args) {
-
-
+        /**
+         * The Facade Pattern is a structural design pattern that
+         * provides a simple interface to a complex system.
+         * It hides the complexities of subsystems and provides a unified API for clients to interact with.
+         * When to Use the Facade Pattern?
+         * .When you want to simplify interaction with a complex system.
+         * .When you need to decouple clients from multiple subsystem classes.
+         * .When you want to improve maintainability by reducing dependencies.
+         */
     }
 }
